1ff3c572745ae552c7c606461a790ae5ede3ad27
Aamir Nazeer <aamirnazeerbhat@gmail.com>
2023-01-04 17:55:31 +0530

    184

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -40,9 +40,8 @@ router.post(
         id: user.id,
         email: user.email,
       },
-      'asds'
+      process.env.JWT_KEY!
     );
-
     //store it on session object
     req.session = {
       jwt: userJwt,
