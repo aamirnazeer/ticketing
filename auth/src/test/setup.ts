825c16a4e6e795036dc8594ca7e5784106f018e9
Aamir Nazeer <aamirnazeerbhat@gmail.com>
2023-01-04 23:49:32 +0530

    207

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -2,8 +2,10 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
 
-let mongo:any
+let mongo: any;
+
 beforeAll(async () => {
+  process.env.JWT_KEY = 'asdfsd';
   const mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
 
