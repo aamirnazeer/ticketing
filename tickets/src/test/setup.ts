32424f6eee0f5f15d2eda9d9b8903363077c1a42
Aamir Nazeer <aamirnazeerbhat@gmail.com>
2023-01-09 13:05:40 +0530

    280

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,9 +2,10 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../app';
+import jwt from 'jsonwebtoken';
 
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: any;
@@ -31,19 +32,25 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  const email = 'test@test.com';
-  const password = 'password';
+global.signin = () => {
+  // build a json webtocket payload {id, email}
+  const payload = {
+    id: 'asdfasdfasdf',
+    email: 'test@test.com',
+  };
 
-  const response = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email,
-      password,
-    })
-    .expect(201);
+  // create the jwt!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  const cookie = response.get('Set-Cookie');
+  // build session object {jwt: MY_JWT}
+  const session = { jwt: token };
 
-  return cookie
+  // turn that session into json
+  const sessionJSON = JSON.stringify(session);
+
+  // take json and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // return a string that is a cookie with a encoded data
+  return [`session=${base64}`];
 };
