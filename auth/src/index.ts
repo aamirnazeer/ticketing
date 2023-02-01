bba4b343c9ad2629ceb290fe327c2434291cdb06
Aamir Nazeer <aamirnazeerbhat@gmail.com>
2023-02-01 14:28:31 +0530

    again auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Starting up the Auth Service...!');
+  console.log('Starting up the Auth Service..!');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY is not defined');
   }
